fe8f2d177fff0af64316326a1d84665c52431373
rktakk <166879695+rktakk@users.noreply.github.com>
2024-05-23 17:49:48 -0400

    homework_4.sql

--- 03_homework/homework_4.sql
+++ 03_homework/homework_4.sql
@@ -17,11 +17,6 @@ The `||` values concatenate the columns into strings.
 Edit the appropriate columns -- you're making two edits -- and the NULL rows will be fixed. 
 All the other rows will remain the same.) */
 
-SELECT 
-    COALESCE(product_name, '') || ', ' || COALESCE(product_size, '') || ' (' || COALESCE(product_qty_type, 'unit') || ')'
-FROM 
-    product;
-
 --Windowed Functions
 /* 1. Write a query that selects from the customer_purchases table and numbers each customer’s  
 visits to the farmer’s market (labeling each market date with a different number). 
@@ -32,43 +27,12 @@ each new market date for each customer, or select only the unique market dates p
 (without purchase details) and number those visits. 
 HINT: One of these approaches uses ROW_NUMBER() and one uses DENSE_RANK(). */
 
-SELECT
-    customer_id,
-    market_date,
-    ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date) AS visit_number
-FROM
-    customer_purchases;
-
 /* 2. Reverse the numbering of the query from a part so each customer’s most recent visit is labeled 1, 
 then write another query that uses this one as a subquery (or temp table) and filters the results to 
 only the customer’s most recent visit. */
 
-WITH NumberedVisits AS 
-    (SELECT
-        customer_id,
-        market_date,
-        ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY market_date DESC) AS visit_number_reverse
-    FROM
-        customer_purchases)
-
-SELECT
-    customer_id,
-    market_date
-FROM
-    NumberedVisits
-WHERE
-    visit_number_reverse = 1;
-
 /* 3. Using a COUNT() window function, include a value along with each row of the 
 customer_purchases table that indicates how many different times that customer has purchased that product_id. */
-
-SELECT
-    customer_id,
-    market_date,
-	product_id,
-    count() OVER (PARTITION BY product_id ORDER BY customer_id desc) AS purchase_history
-FROM
-    customer_purchases;
 
 -- String manipulations
 /* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
@@ -82,22 +46,7 @@ Remove any trailing or leading whitespaces. Don't just use a case statement for 
 
 Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
 
-SELECT
-    product_name,
-    TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1)) AS description
-FROM
-    product;
-
 /* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-SELECT
-    product_name,
-    TRIM(SUBSTR(product_name, INSTR(product_name, '-') + 1)) AS description,
-    product_size
-FROM
-    product
-WHERE
-    product_size REGEXP '[0-9]';
 
 -- UNION
 /* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
@@ -108,7 +57,3 @@ HINT: There are a possibly a few ways to do this query, but if you're struggling
 "best day" and "worst day"; 
 3) Query the second temp table twice, once for the best day, once for the worst day, 
 with a UNION binding them. */
-
-Not completed yet
-
-
